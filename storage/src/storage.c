e85cd61e27bf6ccb01f3c2bdbf7095c7f8d3f600
curtiFacundo <fcurti@frba.utn.edu.ar>
2025-09-16 23:01:53 +0000

    agrego creacion de fs con fresh start

--- storage/src/storage.c
+++ storage/src/storage.c
@@ -4,6 +4,13 @@ int main(int argc, char* argv[]) {
     pthread_t tid_server_mh_worker;
 
     levantarConfig();
+    if (strcmp(fresh_start, "TRUE") == 0){
+        borrar_directorio(punto_montaje);
+        crear_directorio(punto_montaje);
+    }else{
+        crear_directorio(punto_montaje);
+    }
+
     pthread_create(&tid_server_mh_worker, NULL, server_mh_worker, NULL);
     pthread_join(tid_server_mh_worker, NULL);
 
@@ -16,8 +23,10 @@ void levantarConfig(){
     config = config_create("./storage.config");
     char *value = config_get_string_value(config, "LOG_LEVEL");
     current_log_level = log_level_from_string(value);
-    puerto= config_get_string_value(config, "PUERTO_ESCUCHA");
+    puerto = config_get_string_value(config, "PUERTO_ESCUCHA");
     logger = log_create("storage.log", "STORAGE", 1, current_log_level);
+    fresh_start = config_get_string_value(config, "FRESH_START");
+    punto_montaje = config_get_string_value(config, "PUNTO_MONTAJE");
 
 }
 
@@ -42,6 +51,79 @@ void *server_mh_worker(void *args){ // Server Multi-hilo
         log_info(logger, "Se conecta un Worker con Id: <%d> ",parametros_recibidos_worker.id);
         enviar_paquete_ok(socket_nuevo);
            
-        }
-        return (void *)EXIT_SUCCESS;
     }
+    return (void *)EXIT_SUCCESS;
+}
+
+char* crear_directorio(char* path_a_crear) {
+    if (!path_a_crear) {
+        log_error(logger, "Error: La ruta base es NULL.\n");
+        return NULL;
+    }
+
+    char* ruta = strdup(path_a_crear);
+    if (!ruta) {
+        log_error(logger, "Error: No se pudo asignar memoria para la ruta del directorio.\n");
+        return NULL;
+    }
+
+    if (mkdir(ruta, 0700) == 0) {
+        log_debug(logger,"Directorio '%s' creado correctamente.\n", ruta);
+    } else if (errno == EEXIST) {
+        log_debug(logger,"El directorio '%s' ya existe.\n", ruta);
+    } else {
+        log_error(logger,"Error al crear el directorio");
+        free(ruta);
+        return NULL;
+    }
+
+    return ruta;
+}
+
+char* borrar_directorio(const char* path_a_borrar) {
+    if (!path_a_borrar) {
+        log_error(logger, "Error: La ruta a borrar es NULL.\n");
+        return NULL;
+    }
+
+    DIR* dir = opendir(path_a_borrar);
+    if (!dir) {
+        if (errno == ENOENT) {
+            log_debug(logger, "El directorio '%s' no existe.\n", path_a_borrar);
+        } else {
+            log_error(logger, "No se pudo abrir el directorio '%s'.\n", path_a_borrar);
+        }
+        return NULL;
+    }
+
+    struct dirent* entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            continue;
+        if (strcmp(entry->d_name, "superblock.config") == 0)
+            continue;
+        char* ruta_completa = malloc(strlen(path_a_borrar) + strlen(entry->d_name) + 2);
+        sprintf(ruta_completa, "%s/%s", path_a_borrar, entry->d_name);
+
+        struct stat st;
+        if (stat(ruta_completa, &st) == 0) {
+            if (S_ISDIR(st.st_mode)) {
+                borrar_directorio(ruta_completa);
+                if (rmdir(ruta_completa) != 0) {
+                    log_error(logger, "No se pudo borrar el directorio '%s'.\n", ruta_completa);
+                } else {
+                    log_debug(logger, "Directorio '%s' borrado.\n", ruta_completa);
+                }
+            } else {
+                if (remove(ruta_completa) != 0) {
+                    log_error(logger, "No se pudo borrar el archivo '%s'.\n", ruta_completa);
+                } else {
+                    log_debug(logger, "Archivo '%s' borrado.\n", ruta_completa);
+                }
+            }
+        }
+        free(ruta_completa);
+    }
+    closedir(dir);
+    return strdup(path_a_borrar);
+}
--- storage/src/storage.h
+++ storage/src/storage.h
@@ -4,15 +4,26 @@
 #include <utils/utils.h>
 #include <commons/log.h>
 #include <commons/config.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 t_log *logger;
 t_config *config;
 t_log_level current_log_level;
 char * puerto;
+char * fresh_start;
+char * punto_montaje;
 
 int main(int argc, char* argv[]);
 void levantarConfig();
 void* server_mh_worker(void *args);
+char* crear_directorio(char* path_a_crear);
+char* borrar_directorio(const char* path_a_borrar);
 
 
 #endif
