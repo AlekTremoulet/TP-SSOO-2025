2001b03ceb220ac23f87e7f0b4007f346d8c316f
curtiFacundo <fcurti@frba.utn.edu.ar>
2025-09-20 20:16:41 +0000

    funcion cargar en fs

--- storage/src/storage.c
+++ storage/src/storage.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
     }else{
         crear_directorio(punto_montaje);
     }
-
+    levantarConfigSuperblock();
     pthread_create(&tid_server_mh_worker, NULL, server_mh_worker, NULL);
     pthread_join(tid_server_mh_worker, NULL);
 
@@ -28,6 +28,13 @@ void levantarConfig(){
     fresh_start = config_get_string_value(config, "FRESH_START");
     punto_montaje = config_get_string_value(config, "PUNTO_MONTAJE");
 
+}
+
+
+void levantarConfigSuperblock(){
+    tam_fs = config_get_int_value(config, "FS_SIZE");
+    tam_bloque = config_get_int_value(config, "BLOCK_SIZE");
+    
 }
 
 
@@ -126,4 +133,36 @@ char* borrar_directorio(const char* path_a_borrar) {
     }
     closedir(dir);
     return strdup(path_a_borrar);
+}
+
+int cargar_en_fs(char nombre_archivo, char* ruta_base){ 
+
+    if (ruta_base == NULL) {
+        log_error(logger, "No se pudo obtener la ruta base para el dump");
+        return -1;
+    }
+
+    size_t ruta_completa_len = strlen(ruta_base) + strlen(nombre_archivo) + 2;
+    char* ruta_completa = malloc(ruta_completa_len);
+    if (!ruta_completa) {
+        log_error(logger, "No se pudo asignar memoria para la ruta completa");
+        free(ruta_base);
+        return -1;
+    }
+
+    snprintf(ruta_completa, ruta_completa_len, "%s/%s", ruta_base, nombre_archivo);
+
+    FILE* f = fopen(ruta_completa, "w");
+    if (!f) {
+        log_error(logger, "No se pudo abrir archivo");
+        free(ruta_base);
+        free(ruta_completa);
+        return -1;
+    }
+    
+    fclose(f);
+    free(ruta_base);
+    free(ruta_completa);
+
+    return 0;
 }
--- storage/src/storage.h
+++ storage/src/storage.h
@@ -18,9 +18,13 @@ t_log_level current_log_level;
 char * puerto;
 char * fresh_start;
 char * punto_montaje;
+int * tam_fs;
+int * tam_bloque;
+
 
 int main(int argc, char* argv[]);
 void levantarConfig();
+void levantarConfigSuperblock();
 void* server_mh_worker(void *args);
 char* crear_directorio(char* path_a_crear);
 char* borrar_directorio(const char* path_a_borrar);
