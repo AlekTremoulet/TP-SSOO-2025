c8a8af06b4ceabfccea2a3efeb34b4cad92ba566
santi12386 <santiagoozan97@gmail.com>
2025-12-18 17:41:25 -0300

    anti crash query_end

--- master/src/master_query.c
+++ master/src/master_query.c
@@ -56,6 +56,23 @@ void query_enviar_lectura(query_t * q, char * archivo, char * tag, char * buffer
     enviar_paquete(paquete_send, q->socket_qc);
 }
 void query_finalizar(query_t * q, char* motivo){
+    char tmp;
+    ssize_t ret = recv(q->socket_qc, &tmp, 1, MSG_PEEK | MSG_DONTWAIT);
+    if (ret == 0) {
+        // Peer closed connection
+        close(q->socket_qc);
+        
+        finalizar_worker_query(q);
+
+        return; // skip enviar_paquete
+    } else if (ret < 0 && errno != EAGAIN && errno != EWOULDBLOCK) {
+        // Real error occurred
+        log_error(logger, "socket check failed");
+        close(q->socket_qc);
+        finalizar_worker_query(q);
+        return;
+    }
+    
     t_paquete * paquete_end = crear_paquete(QUERY_FINALIZACION);
     agregar_a_paquete(paquete_end, motivo, strlen(motivo)+1);
     enviar_paquete(paquete_end, q->socket_qc);
